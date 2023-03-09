947a3a8546086ca298891a271ae0217de47af401
КутдусовД <@223-7>
2023-03-09 21:30:52 +0600

    init commit

---
+++ mainapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from mainapp.models import Product, ProductCategory
+
+
+def products(request):
+    title = "продукты"
+    products = Product.objects.all()[:2]
+    categories = ProductCategory.objects.all()
+    context = {
+        'title': title,
+        'products': products,
+        'categories': categories,
+    }
+    # return render(request=request, template_name='products.html', context=context)
+    return render(request, 'products.html', context)
+
---
+++ stepshop/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from .views import index, contacts, about
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('contacts/', contacts, name='contacts'),
+    path('about/', about, name='about'),
+    path('products/', include('mainapp.urls', namespace='products')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
